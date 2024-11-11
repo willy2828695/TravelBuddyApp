4ad7d92ace48dfbbc4eb47a48b5b5fd80534a68b
stanley <coronavirusvaccinexx1@gmail.com>
2024-11-11 13:47:57 -0500

    initial project

---
+++ src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		Button btOK = new Button("Hello World");
+		Scene scene = new Scene(btOK, 200, 250);
+		primaryStage.setTitle("MyJavaFX");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
